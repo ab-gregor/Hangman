67f37bbd26165d222b650c8c1f4d0c7ced90d5a5
Sagar R.B <sagarrb1805@gmail.com>
2023-04-27 14:26:04 +0530

    category

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -19,29 +19,42 @@ namespace Hangman
             Console.WriteLine("| Welcome to Hangman |");
             Console.WriteLine(" --------------------");
             string[] listwords = new string[11];
+            List<String> animals = new List<String>();
+            List<String> vehicles= new List<String>();
+            List < KeyValuePair<string, List<string>>> CategoryWords=new List<KeyValuePair<string, List<string>>>();
+            CategoryWords.Add(new KeyValuePair<string, List<string>>("Animals",animals));
+            CategoryWords.Add(new KeyValuePair<string, List<string>>("Vehicles", vehicles));
+            animals.Add("dinosaur");
+            animals.Add("chameleon");
+            animals.Add("iguana");
+            vehicles.Add("volkswagen");
+            vehicles.Add("ford");
+            vehicles.Add("bmw");
+            Console.WriteLine("Select Category");
+            Console.WriteLine("1.Animals \n 2.Vehicles");
+            int ch=Convert.ToInt32(Console.ReadLine());
             List<string> words = new List<string>();
-            words.Add("dinosaur");
-            words.Add("scheme");
-            words.Add("marlboro");
-            words.Add("ignorant");
-            words.Add("thiruvananthapuram");
-            words.Add("synthetic");
-            words.Add("claustrophobia");
-            words.Add("gibberish");
-            words.Add("stethoscope");
-            words.Add("butterscotch");
+            if(ch==1)
+            {
+                words = animals;
+            }
+            else if (ch == 2)
+            {
+                words= vehicles;
+            }
+            
             bool flag;
             Random randGen = new Random();
             var idx = randGen.Next(0, words.Count);
             string mysteryWord = words[idx];
-            Console.WriteLine("It is a {0} letter word",mysteryWord.Length);
+            Console.WriteLine("It is a {0} letter word", mysteryWord.Length);
             char[] guess = new char[mysteryWord.Length];
             Console.Write("Please enter your guess: ");
             int numGuess = 0;
             for (int p = 0; p < mysteryWord.Length; p++)
                 guess[p] = '-';
 
-            while (numGuess<=mysteryWord.Length)
+            while (numGuess <= mysteryWord.Length)
             {
                 flag = false;
                 char playerGuess = char.Parse(Console.ReadLine());
@@ -54,14 +67,14 @@ namespace Hangman
                         flag = true;
                         //Console.WriteLine(flag);
                     }
-                    
-                    
+
+
                 }
-               // Console.WriteLine(flag);
+                // Console.WriteLine(flag);
                 if (flag == false) { numGuess++; }
                 Console.WriteLine(guess);
-                String s= new String(guess);
-                if(s==(mysteryWord))
+                String s = new String(guess);
+                if (s == (mysteryWord))
                 {
                     Score.score.Add(100 - numGuess);
                     Console.WriteLine(" ------------------------------------");
@@ -82,10 +95,10 @@ namespace Hangman
                     {
                         Environment.Exit(1);
                     }
-                    
+
                 }
-                
-                if (numGuess == mysteryWord.Length )
+
+                if (numGuess == mysteryWord.Length)
                 {
                     Console.WriteLine(" ------------------------------------");
                     Console.WriteLine("!!!!!Game Over!!!!!!");
@@ -103,9 +116,9 @@ namespace Hangman
                         Environment.Exit(1);
                     }
                 }
-                
+
             }
-            
+
         }
     }
 }
